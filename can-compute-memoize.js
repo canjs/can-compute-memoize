69336d5557a94839afd1dedfa1f5bff44f9dadc3
imasustink <austin@austinkurpuis.com>
2017-09-20 22:06:53 -0700

    Address Justin's concerns with the current implementation

--- can-compute-memoize.js
+++ can-compute-memoize.js
@@ -1,53 +1,67 @@
 var compute = require('can-compute');
-var weakMap = new WeakMap();
+
+function getWeakMap(){
+	if(typeof WeakMap === 'function'){
+		return new WeakMap();
+	}
+	console.warn('This browser does not support WeakMap, can-compute-memoize has been deoptimized.');
+}
+
+function createCompute(context, args, toValue){
+	return compute(function () {
+		return toValue.apply(context, args);
+	});
+}
+
+var memoizeCache = getWeakMap();
 
 module.exports = function (context, name, args, toValue) {
 	var lastArg = args[args.length - 1];
 	var cache;
 
-	if (weakMap.has(context)) {
-		cache = weakMap.get(context);
-	} else {
-		cache = {};
-		weakMap.set(context, cache);
-	}
-
-	var memoizedArgs = cache[name];
-
-	if (!memoizedArgs) {
-		memoizedArgs = cache[name] = new Map();
-	}
-
-	for (var i = 0; i < args.length; i++) {
-		var arg = args[i];
-		if (memoizedArgs.has(arg)) {
-			memoizedArgs = memoizedArgs.get(arg);
-			continue;
+	if(typeof memoizeCache !== 'undefined'){
+		if (memoizeCache.has(context)) {
+			cache = memoizeCache.get(context);
+		} else {
+			cache = {};
+			memoizeCache.set(context, cache);
 		}
-		var map = new Map();
-		memoizedArgs.set(arg, map);
-		memoizedArgs = map;
-	}
-
-	if (!memoizedArgs.has(lastArg)) {
-		var c = compute(function () {
-			return toValue.apply(context, args);
-		});
-		var eventTeardown = c.computeInstance._eventTeardown;
-		c.computeInstance._eventTeardown = function () {
-			eventTeardown.call(c.computeInstance);
-			delete cache[name];
-			if (!Object.keys(cache).length) {
-				weakMap.delete(context);
+	
+		var memoizedArgs = cache[name];
+	
+		if (!memoizedArgs) {
+			memoizedArgs = cache[name] = new Map();
+		}
+	
+		for (var i = 0; i < args.length; i++) {
+			var arg = args[i];
+			if (memoizedArgs.has(arg)) {
+				memoizedArgs = memoizedArgs.get(arg);
+				continue;
 			}
-		};
-		compute.temporarilyBind(c);
-		memoizedArgs.set(lastArg, c);
-	}
+			var map = new Map();
+			memoizedArgs.set(arg, map);
+			memoizedArgs = map;
+		}
 
-	return memoizedArgs.get(lastArg);
+		if (!memoizedArgs.has(lastArg)) {
+			var c = createCompute(context, args, toValue);
+			var eventTeardown = c.computeInstance._eventTeardown;
+			c.computeInstance._eventTeardown = function () {
+				eventTeardown.call(c.computeInstance, arguments);
+				memoizedArgs.delete(lastArg);
+			};
+			compute.temporarilyBind(c);
+			memoizedArgs.set(lastArg, c);
+			return c;
+		}
+
+		return memoizedArgs.get(lastArg);
+	}else{
+		return createCompute(context, args, toValue);
+	}
 };
 
 module.exports.clear = function () {
-	weakMap = new WeakMap();
+	memoizeCache = getWeakMap();
 };
